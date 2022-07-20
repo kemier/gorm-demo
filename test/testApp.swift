cae43a1d4ae7e1cc12514cddf864c36fca0dc8ad
zaedin <zengningbj@163.com>
2022-07-20 13:55:08 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 曾宁 on 2022/7/20.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
